ad5216cb60cb0a64f37e5b89fbedbee6dc7c5275
SteveEasley <steve.easley@cbsinteractive.com>
2016-06-30 15:50:39 -0400

    Fix env

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-set -e
-
-DOCKER_MACHINE_IP=$(ip route show 0.0.0.0/0 | grep -Eo 'via \S+' | awk '{ print $2 }') php-fpm --allow-to-run-as-root
+export DOCKER_MACHINE_IP=$(ip route show 0.0.0.0/0 | grep -Eo 'via \S+' | awk '{ print $2 }')
+php-fpm --allow-to-run-as-root
